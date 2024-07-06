28a0433ce64bc082690973a75a4771cc843583db
SwayamKhatter <khatterswayam@gmail.com>
2024-07-06 17:01:28 +0530

    47

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,18 +7,27 @@ router.get('/', [
     body('name').isLength({min: 3}),
     body('email').isEmail(),
     body('password').isLength({min: 5}),
-], (req, res)=>{
+], async(req, res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
-    User.create({
+
+    try{
+    let user =User.findOne({email: req.body.email});
+    if(user){
+        return res.status(400).json({error: "Sorry a user with this email already exists"})
+    }
+    user= await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
-    }).then(user => res.json(user))
-    .catch(err=> {console.log(err)
-res.json({error: 'Please enter a unique value for email'})})
+    })
+    req.json(user)
+    } catch(error){
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 
 module.exports = router
