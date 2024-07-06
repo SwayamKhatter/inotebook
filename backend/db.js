a5d7f69b6af2c8aa6a121edcdd25d75baf4055c2
SwayamKhatter <khatterswayam@gmail.com>
2024-07-06 16:26:08 +0530

    46

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb+srv://swayamkhatter:eqpb3yu2cd@cluster0.hq7bs1c.mongodb.net/"
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Sucessfull");
-    })
-}
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit the process with failure
+    }
+};
 
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const connectToMongo = require('./db');
 const express = require('express')
-connectToMongo();
 
+connectToMongo();
 const app = express()
 const port = 3000
-app.get('/', (req, res)=>{
-    res.send('Hello World!')
-})
 
-app.listem(port, ()=>{
-    console.log(`Example app listening at http://localhost:${port}`)
+// Available routes
+app.use('/api.auth', require('./routes/auth'))
+app.use('/api.notes', require('./routes/notes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
