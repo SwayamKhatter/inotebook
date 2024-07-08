252cc8279e8bf76e263b8183d204454083ffdb0a
SwayamKhatter <khatterswayam@gmail.com>
2024-07-08 14:43:16 +0530

    63

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -43,17 +43,21 @@
 import React from 'react';
 import { useNotes } from '../context/notes/noteContext';
 import NoteItem from './NoteItem';
+import AddNote from './AddNote';
 
 const Notes = () => {
   const { notes } = useNotes();
 
   return (
+    <>
+    <AddNote/>
     <div className="row my-3">
       <h2>Your Notes</h2>
       {notes.map((note) => (
         <NoteItem key={note._id} note={note} />
       ))}
     </div>
+    </>
   );
 };
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -66,6 +66,33 @@ const NoteState = (props) => {
 
     const [notes, setNotes] = useState(notesInitial);
 
+    // Add a Note
+    const addNote = (title, description, tag) => {
+        // TODO: API Call
+        console.log("Adding a new Note");
+        const note = {
+            "_id": "668b844589bdd2c5de741716",
+            "user": "668b7766b4501faef3f90ece",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "__v": 0
+        };
+        setNotes(notes.concat(note));
+    }
+
+    // Delete a Note
+    const deleteNote = (id) => {
+        // TODO: API Call
+        console.log("Deleting the note with id:" + id);
+    }
+
+    // Edit a Note
+    const editNote = (id, title, description, tag) => {
+        // TODO: API Call
+        console.log("Editing the note with id:" + id);
+    }
+
     return (
         <NoteContext.Provider value={{ notes, setNotes }}>
             {props.children}
